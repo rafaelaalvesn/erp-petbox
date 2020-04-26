1865583d50d6ff3964d5e7f68b8e5ee340eaff48
rafaelaalvesn <rafaelaalvesn@hotmail.com>
2020-04-26 17:14:29 -0300

    relatórios financeiros

--- relatorios-financeiro.js
+++ relatorios-financeiro.js
@@ -32,19 +32,39 @@ $('#btnGerarRelatorio').click(function () {
         ano = 2020;
     }
 
-    switch (relatorio) {
-        case 'facebook':
-            BuscaAlcanceFacebook(mes, ano);
-            break;
-        case 'instagram':
-            BuscaSeguidoresInstagram();
-            break;
-        case 'comunicacao':
-            BuscaMeioDeComunicacao();
-            break;
-        default:
-            break;
-    }
+    GeraRelatorio();
+
+    // switch (relatorio) {
+    //     case '1':
+    //         GeraRelatorio(1);
+    //         break;
+    //     case '2':
+    //         GeraRelatorio(2);
+    //         break;
+    //     case '3':
+    //         GeraRelatorio(3);
+    //         break;
+    //     case '4':
+    //         GeraRelatorio(4);
+    //         break;
+    //     case '5':
+    //         GeraRelatorio(5);
+    //         break;
+    //     case '6':
+    //         GeraRelatorio(6);
+    //         break;
+    //     case '7':
+    //         GeraRelatorio(7);
+    //         break;
+    //     case '8':
+    //         GeraRelatorio(8);
+    //         break;
+    //     case '9':
+    //         GeraRelatorio(9);
+    //         break;
+    //     default:
+    //         break;
+    // }
 
     sessionStorage.setItem("mesRelatorio", null)
     sessionStorage.setItem("anoRelatorio", null)
@@ -53,9 +73,14 @@ $('#btnGerarRelatorio').click(function () {
 });
 
 
-async function BuscaAlcanceFacebook(mes, ano) {
+async function GeraRelatorio(val) {
 
-    var usuarios = [1000, 515, 15250, 222];
+    var val_1 = Math.random() * (10000 - 1) + 1;
+    var val_2 = Math.random() * (10000 - 1) + 1;
+    var val_3 = Math.random() * (10000 - 1) + 1;
+    var val_4 = Math.random() * (10000 - 1) + 1;
+
+    var usuarios = [val_1, val_2, val_3, val_4];
 
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
@@ -63,36 +88,20 @@ async function BuscaAlcanceFacebook(mes, ano) {
         data: {
             labels: ['Jan', 'Fev', 'Mar', 'Abr'],
             datasets: [{
-                label: ' pessoas',
+                label: ' reais (R$)',
                 data: usuarios,
-                // backgroundColor: [
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)',
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)',
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)'
-                // ],
-                // borderColor: [
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)',
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)',
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)'
-                // ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
                 borderWidth: 1
             }]
         },
@@ -110,7 +119,7 @@ async function BuscaAlcanceFacebook(mes, ano) {
 
 async function BuscaSeguidoresInstagram() {
 
-     var seguidores = [10, 55, 150, 200, 225, 278, 310, 485, 525, 687, 700, 1000];
+    var seguidores = [10, 55, 150, 200, 225, 278, 310, 485, 525, 687, 700, 1000];
 
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
@@ -187,9 +196,9 @@ async function BuscaMeioDeComunicacao() {
     });
 }
 
-$("#print").click(async function(){
+$("#print").click(async function () {
 
     $('#navigation').remove();
     window.print();
-    window.document.location = "./relatorios-mkt.html";
+    window.document.location = "./relatorios-financeiro.html";
 })
--- rh.js
+++ rh.js
@@ -6,6 +6,8 @@ $('#funcionarios').click(async function () {
 
     await $.getJSON('json/funcionarios.json', async function (data) {
 
+        localStorage.setItem("funcionarios", JSON.stringify(data));
+
         var html = '<div>' +
             '<h3 style="margin-left:1%">Funcionários</h3>' +
             '<hr>' +
@@ -21,6 +23,8 @@ $('#funcionarios').click(async function () {
                 `<td>${data[index].FUNCAO}</td>` +
                 `<td>${data[index].SALARIO}</td>` +
                 `<td>${data[index].DATA_ADMISSAO}</td>` +
+                `<td><i class="fa fa-pencil" aria-hidden="true" onclick="EditarFuncionario(${data[index].ID}})"></i></td>` +
+                `<td><i class="fa fa-trash-o" aria-hidden="true" onclick="RemoverFuncionario(${data[index].ID})"></i></td>` +
                 `</tr>`
         }
 
@@ -33,6 +37,8 @@ $('#funcionarios').click(async function () {
             '<th scope="col">FUNÇÃO</th>' +
             '<th scope="col">SALÁRIO</th>' +
             '<th scope="col">DATA DE ADMISSÃO</th>' +
+            '<th scope="col"></th>' +
+            '<th scope="col"></th>' +
             '</tr>' +
             '</thead>' +
             '<tbody>' +
@@ -48,28 +54,27 @@ $('#funcionarios').click(async function () {
 $('#folhaDePonto').click(async function () {
 
     window.document.location = "./folhaDePonto.html";
-        //MontaHTML();
+    //MontaHTML();
     //var funcionario = $('#funcionarioSelecionado:selected').text();
     //await PreencheFolhaDePonto(funcionario)
 });
 
 $('#funcionarioSelecionado').change(async function () {
 
-    var funcionario=$(this).val();
+    var funcionario = $(this).val();
     $('#tableFolhaDePonto').remove();
-  
-   var numMes= $('#mesReferencia').val();
-   var descMes = $("#mesReferencia option:selected").text();
 
-   PreencheFolhaDePonto(funcionario, numMes, descMes);
-    
-    
+    var numMes = $('#mesReferencia').val();
+    var descMes = $("#mesReferencia option:selected").text();
+
+    PreencheFolhaDePonto(funcionario, numMes, descMes);
+
+
 });
 
 
-async function PreencheFolhaDePonto(funcionario, numMes, descMes)
-{
-    await $.getJSON('json/folhaDePonto'+funcionario+'.json', async function (data) {
+async function PreencheFolhaDePonto(funcionario, numMes, descMes) {
+    await $.getJSON('json/folhaDePonto' + funcionario + '.json', async function (data) {
 
         var html = ""
         for (let index = 0; index < data.length; index++) {
@@ -104,4 +109,59 @@ async function PreencheFolhaDePonto(funcionario, numMes, descMes)
     })
 
     //await alert(funcionario);
+}
+
+async function RemoverFuncionario(id, data) {
+
+    var excluir = confirm("Tem certeza que deseja excluir?")
+
+    if (excluir) {
+        $("#bodyVendas").html("");
+        await $.getJSON('json/funcionarios.json', async function (data) {
+    
+            var html = '<div>' +
+                '<h3 style="margin-left:1%">Funcionários</h3>' +
+                '<hr>' +
+                '</div>'
+    
+            for (let index = 0; index < data.length; index++) {
+    
+                if (data[index].ID != id) {
+                    html += `<tr>` +
+                        `<th scope="row">${data[index].ID}</th>` +
+                        `<td>${data[index].NOME}</td>` +
+                        `<td>${data[index].SETOR}</td>` +
+                        `<td>${data[index].FUNCAO}</td>` +
+                        `<td>${data[index].SALARIO}</td>` +
+                        `<td>${data[index].DATA_ADMISSAO}</td>` +
+                        `<td><i class="fa fa-pencil" aria-hidden="true" onclick="EditarFuncionario(${data[index].ID})"></i></td>` +
+                    `<td><i class="fa fa-trash-o" aria-hidden="true" onclick="RemoverFuncionario(${data[index].ID})"></i></td>` +
+                        `</tr>`
+                }
+    
+            }
+    
+            $("#bodyVendas").html('<table class="table table-hover">' +
+                ' <thead>' +
+                '<tr>' +
+                '<th scope="col">ID</th>' +
+                '<th scope="col">NOME</th>' +
+                '<th scope="col">SETOR</th>' +
+                '<th scope="col">FUNÇÃO</th>' +
+                '<th scope="col">SALÁRIO</th>' +
+                '<th scope="col">DATA DE ADMISSÃO</th>' +
+                '<th scope="col"></th>' +
+                '<th scope="col"></th>' +
+                '</tr>' +
+                '</thead>' +
+                '<tbody>' +
+                html +
+    
+                '</tbody>' +
+                '</table>'
+            )
+        })
+    }
+    else return
+   
 }
